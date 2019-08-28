216dd9af1ccafc7f8848bda5971957b4f61b129d
adelashraf <adelattttt@gmail.com>
2019-08-28 22:08:02 +0200

    3rd

--- app/src/main/java/com/onebound/myapplication/MainActivity.kt
+++ app/src/main/java/com/onebound/myapplication/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this,"adel",Toast.LENGTH_LONG).show()
+        Toast.makeText(this,"kamal",Toast.LENGTH_LONG).show()
     }
 }
