53b12fef8983880672c500a62771f58307cbff63
AdelKamalll <51914773+AdelKamalll@users.noreply.github.com>
2019-08-29 19:10:18 +0200

    Update MainActivity.kt

--- app/src/main/java/com/onebound/myapplication/MainActivity.kt
+++ app/src/main/java/com/onebound/myapplication/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this,"kamall",Toast.LENGTH_LONG).show()
+        Toast.makeText(this,"aaaa",Toast.LENGTH_LONG).show()
     }
 }
